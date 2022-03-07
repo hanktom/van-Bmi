d1cf1cf6e81eeb2e7117755a4774ec45b8594c62
Hank Tom <hanktom@gmail.com>
2022-03-07 16:39:15 +0800

    lambda and Listener

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -2,24 +2,44 @@ package com.tom.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.tom.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    var secret = (1..10).random()
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(binding.root)
-        setContentView(R.layout.activity_main)
-        val inputNumber = findViewById<EditText>(R.id.number)
-        val data = inputNumber.text.toString()
+        setContentView(binding.root)
+       /* val inputNumber = findViewById<EditText>(R.id.number)
+        val data = inputNumber.text.toString()*/
+        Log.d("MainActivity", "secret: $secret")
 
     }
 
     fun guess(view: View) {
-        println("Hahahaha")
-        println(binding.number.text.toString())
+        counter++
+        binding.tvCounter.text = counter.toString()
+        val num = binding.number.text.toString().toInt()
+        val message = if (num < secret) "Bigger"
+            else if (num > secret) "Smaller"
+            else {
+                secret = (1..10).random()
+                counter = 0
+                "You got it"
+            }
+        AlertDialog.Builder(this)
+                .setTitle("Guess")
+                .setMessage(message)
+                .setPositiveButton("OK" ) {d , w ->
+
+                }
+                .show()
     }
 }
--- lib/src/main/java/com/tom/lib/GraduateStudent.kt
+++ lib/src/main/java/com/tom/lib/GraduateStudent.kt
@@ -2,9 +2,12 @@ package com.tom.lib
 
 class GraduateStudent(name: String, english: Int, math: Int)
                             : Student(name, english, math) {
+    constructor(name: String) : this(name, 0, 0)
+    constructor() : this("", 0, 0)
+
 }
 
 fun main() {
-    var jenny = GraduateStudent("Jenny", 70, 90)
+    var jenny = GraduateStudent("Jenny")
     jenny.print()
 }
