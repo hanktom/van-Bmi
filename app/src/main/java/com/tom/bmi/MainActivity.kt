844f291c6eeecfa994532ea12a0573e622ce1320
Hank Tom <hanktom@gmail.com>
2022-03-08 10:42:46 +0800

    Refactor to M.V.C.

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -10,36 +10,27 @@ import com.tom.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    var secret = (1..10).random()
-    var counter = 0
+    val game = NumberGame()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
         setContentView(binding.root)
-       /* val inputNumber = findViewById<EditText>(R.id.number)
-        val data = inputNumber.text.toString()*/
-        Log.d("MainActivity", "secret: $secret")
-
     }
 
     fun guess(view: View) {
-        counter++
-        binding.tvCounter.text = counter.toString()
         val num = binding.number.text.toString().toInt()
-        val message = if (num < secret) "Bigger"
-            else if (num > secret) "Smaller"
-            else {
-                secret = (1..10).random()
-                counter = 0
-                "You got it"
-            }
         AlertDialog.Builder(this)
-                .setTitle("Guess")
-                .setMessage(message)
-                .setPositiveButton("OK" ) {d , w ->
+            .setTitle("Guess")
+            .setMessage(game.guess(num))
+            .setPositiveButton("OK" ) {d , w ->
+                if (game.end) game.reset()
+                updateUI()
+            }
+            .show()
+        updateUI()
+    }
 
-                }
-                .show()
+    private fun updateUI() {
+        binding.tvCounter.text = game.counter.toString()
     }
 }
