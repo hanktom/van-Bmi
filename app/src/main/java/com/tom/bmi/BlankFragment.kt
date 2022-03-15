895d4b7598f7af9743e62fdab8f1e3c2aada22ee
Hank Tom <hanktom@gmail.com>
2022-03-15 11:25:24 +0800

    migrate to Fragment

--- app/src/main/java/com/tom/bmi/BlankFragment.kt
+++ app/src/main/java/com/tom/bmi/BlankFragment.kt
@@ -4,17 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.tom.bmi.databinding.FragmentBlankBinding
 
 class BlankFragment : Fragment() {
+    lateinit var binding : FragmentBlankBinding
+    val viewModel by viewModels<GuessViewModel>()
+    companion object {
+        val TAG = BlankFragment::class.java.simpleName
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(
-            R.layout.fragment_blank, container, false)
-        return view
+        binding = FragmentBlankBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            val num = binding.number.text.toString().toInt()
+            viewModel.guess(num)
+        }
+        viewModel.counter.observe(viewLifecycleOwner) {
+            binding.tvCounter.setText(it.toString())
+        }
+        viewModel.gameState.observe(viewLifecycleOwner) { state ->
+            val message = when(state) {
+                GuessViewModel.GameState.BIGGER -> getString(R.string.bigger)
+                GuessViewModel.GameState.SMALLER -> getString(R.string.smaller)
+                GuessViewModel.GameState.BINGO -> getString(R.string.bingo)
+                GuessViewModel.GameState.INIT -> "START!"
+                else -> getString(R.string.somehting_goes_wrong)
+            }
+            AlertDialog.Builder(requireContext())
+                .setTitle(getString(R.string.dialog_title))
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.ok) ) { d, w ->
+                    if (state == GuessViewModel.GameState.BINGO)
+                        viewModel.reset()
+                }
+                .show()
+        }
     }
 }
--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -15,36 +15,12 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
     private lateinit var binding : ActivityMainBinding
-    val viewModel by viewModels<GuessViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.counter.observe(this) {
-            binding.tvCounter.setText(it.toString())
-        }
-        viewModel.gameState.observe(this) { state ->
-            val message = when(state) {
-                GuessViewModel.GameState.BIGGER -> getString(R.string.bigger)
-                GuessViewModel.GameState.SMALLER -> getString(R.string.smaller)
-                GuessViewModel.GameState.BINGO -> getString(R.string.bingo)
-                GuessViewModel.GameState.INIT -> "START!"
-                else -> getString(R.string.somehting_goes_wrong)
-            }
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.dialog_title))
-                .setMessage(message)
-                .setPositiveButton(getString(R.string.ok) ) { d, w ->
-                    if (state == GuessViewModel.GameState.BINGO)
-                        viewModel.reset()
-                }
-                .show()
-        }
+
     }
 
-    fun guess(view: View) {
-        val num = binding.number.text.toString().toInt()
-        viewModel.guess(num)
-    }
 }
