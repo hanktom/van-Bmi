b50e99fdec4a65ae5def247ab7e98105c11991d0
Hank Tom <hanktom@gmail.com>
2022-03-15 10:46:55 +0800

    first Fragment

---
+++ app/src/main/java/com/tom/bmi/BlankFragment.kt
@@ -0,0 +1,20 @@
+package com.tom.bmi
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.fragment_blank, container, false)
+        return view
+    }
+}
